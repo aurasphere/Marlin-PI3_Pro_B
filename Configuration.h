034a7350f9b60f22e85ed71b60faf4b5421c9253
Donato Rimenti <donatohan.rimenti@gmail.com>
2023-06-20 08:25:44 +0200

    Set Z_PROBE_OFFSET_FROM_EXTRUDER closer to the dish

--- Configuration.h
+++ Configuration.h
@@ -399,7 +399,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   // X and Y offsets must be integers
   #define X_PROBE_OFFSET_FROM_EXTRUDER 4
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 43
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -1.4
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -2
 
   #define Z_RAISE_BEFORE_HOMING 4       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
